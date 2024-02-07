6027e38d32153e450425c40abdcf274e64dfcd1e
ASIF MALLICK <asifsmallick@gmail.com>
2024-02-07 21:34:32 +0530

    Added patch and delete route

--- route/route.js
+++ route/route.js
@@ -44,4 +44,48 @@ router.post('/post' , async(req , res) => {
     
 })
 
+router.patch('/patch/:id' , async(req , res) => {
+
+    const userid = req.params.id 
+
+    const getdata = await USER.findById({_id : userid})
+
+    if(req.body.first_name != null) { 
+        getdata.first_name = req.body.first_name
+    }  
+
+    if(req.body.last_name != null) {
+        getdata.last_name = req.body.last_name
+    }  
+
+    if(req.body.email != null) {
+        getdata.email = req.body.email
+    } 
+    
+    if(req.body.gender != null) {
+        getdata.gender = req.body.gender
+    }  
+
+    try {
+        const updatedUser = await getdata.save()
+        res.json({updatedUser})
+        
+    } catch (error) {
+
+        res.status(400).json({message : error.message})
+        
+    }
+})
+
+router.delete('/delete/:id' , async(req , res) => {
+
+    try {
+        const deletedata = await USER.deleteOne({_id : req.params.id})
+        res.status(200).json({message : "success"} )
+    } catch (error) {
+        res.status(400).json({message : "something went wrong"})
+    }
+    
+})
+
 module.exports = router
